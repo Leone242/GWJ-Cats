2a63ba849fb324bccb43b89093f2e0e9adfd35e1
Leone242 <leone.tg@outlook.com>
2023-11-13 18:19:58 -0300

    dog patroll - bug

--- dog.gd
+++ dog.gd
@@ -3,16 +3,41 @@ extends CharacterBody2D
 var player
 var speed = 110
 var chase = false
+var OriginPosition 
+var SecondPosition
+var direction = Vector2.ZERO
 
 func _ready():
 	player = get_node("../Node2D/Player")
-
-func _physics_process(delta):
+	OriginPosition = $"../OriginPosition".position
+	SecondPosition = $"../SecondPosition".position
+	self.position = OriginPosition
+	
+	
+func _process(delta):
 	if chase and !player.is_hidden:
-		var direction = (player.position - self.position).normalized()
+		chase_cat()
+	else:
+		patroll()
+		
+		
+func chase_cat():
+	direction = (player.position - self.position).normalized()
+	velocity = direction * speed
+	move_and_slide()
+
+func patroll():
+	if self.position != OriginPosition:
+		await get_tree().create_timer(3.0).timeout
+		direction = (OriginPosition - self.position).normalized()
 		velocity = direction * speed
 		move_and_slide()
-		
+	else:
+		await get_tree().create_timer(3.0).timeout
+		direction = (SecondPosition - self.position).normalized()
+		velocity = direction * speed
+		move_and_slide()
+	
 
 func _on_area_2d_body_entered(body):
 	if body.name == "Player":
@@ -23,3 +48,4 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(body):
 	if body.name == "Player":
 		chase = false
+		patroll()
