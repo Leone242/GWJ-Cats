7a60f16d59ef65d6c7a25724c7571c954bf0f91a
Leone242 <leone.tg@outlook.com>
2023-11-14 14:52:52 -0300

    dog new path - why are you running

--- dog.gd
+++ dog.gd
@@ -3,40 +3,28 @@ extends CharacterBody2D
 var player
 var speed = 110
 var chase = false
-var OriginPosition 
-var SecondPosition
 var direction = Vector2.ZERO
+var path
 
 func _ready():
-	player = get_node("../Node2D/Player")
-	OriginPosition = $"../OriginPosition".position
-	SecondPosition = $"../SecondPosition".position
-	self.position = OriginPosition
+	player = get_node("../../../Node2D/Player")
+	path = get_node("..")
 	
 	
-func _process(delta):
+func _physics_process(delta):
+	patroll(delta)
 	if chase and !player.is_hidden:
 		chase_cat()
-	else:
-		patroll()
-		
+			
 		
 func chase_cat():
 	direction = (player.position - self.position).normalized()
 	velocity = direction * speed
 	move_and_slide()
 
-func patroll():
-	if self.position != OriginPosition:
-		await get_tree().create_timer(3.0).timeout
-		direction = (OriginPosition - self.position).normalized()
-		velocity = direction * speed
-		move_and_slide()
-	else:
-		await get_tree().create_timer(3.0).timeout
-		direction = (SecondPosition - self.position).normalized()
-		velocity = direction * speed
-		move_and_slide()
+func patroll(delta):
+	path.progress += (speed * 0.6) * delta
+	move_and_slide()
 	
 
 func _on_area_2d_body_entered(body):
@@ -48,4 +36,4 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(body):
 	if body.name == "Player":
 		chase = false
-		patroll()
+
